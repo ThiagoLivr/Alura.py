a3e8728fbd67479b8d43620ec5c7f50ae748dfaf
Thiago Livramento Souza <121188925+ThiagoLivr@users.noreply.github.com>
2023-02-23 16:28:27 -0300

    Add files via upload
    Aulas de python. 
    Do basico, ao avançado em orientação a objetos.

---
+++ conta.py
@@ -0,0 +1,42 @@
+
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Contruindo um Objeto... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Olá senhor {}, seu saldo é de R$:{}".format(self.__titular, self.__saldo))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+
+
