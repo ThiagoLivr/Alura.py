97c38165bb639b3aa285952760b692df92cef32c
Thiago Livramento Souza <121188925+ThiagoLivr@users.noreply.github.com>
2023-02-25 13:39:57 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -15,8 +15,15 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, __valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return __valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} é maior que seu limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
@@ -37,6 +44,16 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
+
+
 
 
 
